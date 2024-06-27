d2d50c1e19aa7ea63b872d5a874b6059175cf840
juan666unam <144980080+juan666unam@users.noreply.github.com>
2024-06-26 21:04:47 -0600

    Delete ejercicio01.py

--- ejercicio01.py
+++
@@ -1,8 +0,0 @@
-d = float(input('Indica la distancia entre las 2 ciudades en Km: '))
-va = float(input('Indica la velocidad del vehículo A en Km/h: '))
-vb = float(input('Indica la velocidad del vehículo B en Km/h: '))
-
-xa = va * d / (va + vb)
-xa = xa * 1000
-
-print('La distancia (en metros) donde se dará el choque respecto al punto de partida de A es: ',xa)
