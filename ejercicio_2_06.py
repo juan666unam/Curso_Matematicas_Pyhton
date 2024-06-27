de07145457175a4e8d48cf8f25e102b596138f32
juan666unam <144980080+juan666unam@users.noreply.github.com>
2024-06-26 21:04:35 -0600

    Delete ejercicio_2_06.py

--- ejercicio_2_06.py
+++
@@ -1,7 +0,0 @@
-#Muestra la fizhas de dominó en el formato [m.n] sin que se repitan
-n = 0 #Contador de fichas
-for i in range(7):
-    for j in range(7):
-        if j>=i:
-            n+=1
-            print('Ficha ',n,' : [',i,',',j,']')
